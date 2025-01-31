67ea23753691b23cdb9eb486036eeafdbc9d8aa4
ahaoboy <504595380@qq.com>
2025-02-01 04:58:31 +0800

    add ci

---
+++ cli/cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+if (require('fs').existsSync('../bundle/cli.js')) {
+  require('../bundle/cli.js')
+}
--- cli/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-if (require('fs').existsSync('../bundle/index.js')) {
-  require('../bundle/index.js')
-}
