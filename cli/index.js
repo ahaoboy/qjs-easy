15b2256f2bb408a1d8d0ef28121c8e0eaf5969ca
ahaoboy <504595380@qq.com>
2025-01-31 23:58:57 +0800

    add setup.js

---
+++ cli/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+if (require('fs').existsSync('../bundle/index.js')) {
+  require('../bundle/index.js')
+}
---
+++ cli/setup.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+if (require('fs').existsSync('../bundle/setup.js')) {
+  require('../bundle/setup.js')
+}
